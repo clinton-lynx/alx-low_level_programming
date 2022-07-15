9aea973fb724446e5c767b637bc342d8cfb73c15
clinton-lynx <clintonadeoti1@gmail.com>
2022-07-15 16:20:07 -0700

    task 102

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * * print_number - prints an integer
+ *
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	putchar((num % 10) + '0');
+}
