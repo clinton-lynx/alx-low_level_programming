d7b7af146243c43fa04e1938bf00d83ad079087c
clinton-lynx <clintonadeoti1@gmail.com>
2022-07-16 02:51:08 -0700

    dnddnms

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main(void)
+ /**                                                                                                                                     
+  *          *  * main - Ent                                                                                                                        
+  *
+  *                   *    * Description: 'the program's description'                                                                                        
+  *
+  *                            *      * Return: Always 0 (Success)                                                      
+  *                                     */  
 {
-	/**
-	 *  * main - Ent
-	 *    * Description: 'the program's description'
-	 *      * Return: Always 0 (Success)
-	 */
+
 	puts("Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
