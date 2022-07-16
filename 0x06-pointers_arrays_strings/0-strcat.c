a9fe574760a3d5a7f9b4eebaf618add01897bf7d
clinton-lynx <clintonadeoti1@gmail.com>
2022-07-15 20:37:49 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,38 @@
 #include "main.h"
-/***
- *  * _strcat - concatenates two strings together,
+/**
+ *  *_strcat - concatenates  the string pointed to by @src to
  *
- *   * @dest: destination.
+ *   * the end of the string pointed to by @dest
  *
- *    * @src: source.
+ *    *@dest: String that will be appended
  *
- *     * Return: the pointer to dest.
+ *     *@src: String to be concatenated upon
+ *
+ *      *
+ *
+ *       * Return: returns poiner to @dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-		break;
-		count++;
-		count2++;
-	}
-	return (dest);
+
+
+
+		int index = 0, dest_len = 0;
+
+
+
+			while (dest[index++])
+
+						dest_len++;
+
+
+
+				for (index = 0; src[index]; index++)
+
+							dest[dest_len++] = src[index];
+
+
+
+					return (dest);
+
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,18 +1,13 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
 #include <stdio.h>
-
 /**
- *
  *  *void prototypes()
  *
  *   *int prototypes()
  *
  *    *char prototypes()
- *
- *     */
+ */
 
 void reverse_array(int *a, int n);
 
